ac209e945e8670fc3c0670274d0385476e6e7790
Prelang Builder <builder@prelang.com>
2017-01-06 17:40:04 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Student' to allow for create, update

--- app/admin/student.rb
+++ app/admin/student.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Student do
+  permit_params :first_name, :last_name, :email, :payment_type, :last_four
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
